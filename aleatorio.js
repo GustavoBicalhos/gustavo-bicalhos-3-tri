bd6cb9440992ef6d2da13f6e8f3b37a67d9c495e
GustavoBicalhos <108080563+GustavoBicalhos@users.noreply.github.com>
2024-10-07 10:31:41 -0300

    Update aleatorio.js

--- aleatorio.js
+++ aleatorio.js
@@ -1,7 +1,13 @@
-const nomes = ["Ana", "Fernanda", "Maria Eduarda",
-"Marcelo", "Amanda", "Gustavo", "Gabriel"];
+// aleatorio.js
 
-export function aleatorio (lista){
-const posicao = Math.floor(Math.random()* lista.length);
-return lista[posicao];
+// Lista de nomes
+const nomes = ["Ana", "Fernanda", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+
+// Função que retorna um nome aleatório
+export function aleatorio(lista) {
+    const posicao = Math.floor(Math.random() * lista.length);
+    return lista[posicao];
 }
+
+// Constante que armazena um nome aleatório gerado
+export const nome = aleatorio(nomes);
